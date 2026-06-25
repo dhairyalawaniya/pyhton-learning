3120c91f73f1198944af010f39acefe8c4947978
dhairyalawaniya <dhairyalawaniya@gmail.com>
2026-06-25 14:41:00 +0530

    Add files via upload

---
+++ 25hwfunctions.py
@@ -0,0 +1,23 @@
+#a = "acceptance" 
+#print (len(a))
+
+#a = 'dhairya'
+#print ('D'in a )
+
+
+#a = "khairya"
+#a = "d" + a[1:]
+#print (a)
+
+#a = "elephant"
+#print (a.upper())
+
+#a = "JAIPUR"
+#print (a.lower())
+
+
+#a = "****---japan-#*++"
+#print (a.strip("*-+#"))
+
+a = "www.experience.com"
+print (a.lstrip("w."))
